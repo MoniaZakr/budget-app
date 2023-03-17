eb0c429410d48ca76a0145e8f1e5fd81e948c5e9
Moika Zakrzewska <monikawiniar@gmail.com>
2023-03-17 17:13:08 +0100

    Created 'UI' and 'Expenses' subfolders.

---
+++ src/components/dist/card.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+require("./Card.css");
+
+function Card() {
+  return div.card;
+}
+
+var _default = Card;
+exports["default"] = _default;
+//# sourceMappingURL=card.dev.js.map
