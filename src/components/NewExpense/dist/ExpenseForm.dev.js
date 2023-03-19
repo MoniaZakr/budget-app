a58e9d81d802186a404b95a50419e444eb0297aa
Moika Zakrzewska <monikawiniar@gmail.com>
2023-03-19 07:28:18 +0100

    Form component has been created.

---
+++ src/components/NewExpense/dist/ExpenseForm.dev.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./ExpenseForm.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function ExpenseForm() {
+  return form > div["new"] - expense__controls;
+}
+
+var _default = ExpenseForm;
+exports["default"] = _default;
+//# sourceMappingURL=ExpenseForm.dev.js.map
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,7 @@
 import './Card.css';
 
 function Card(props) {
+    // Main class plus all classes of children elements.
     const classes = 'card ' + props.className;
     return <div className={classes}>{props.children}</div>;
 };
