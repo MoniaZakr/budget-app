9548cf0bf02da5153b790d0161768b73ce17cc2d
Moika Zakrzewska <monikawiniar@gmail.com>
2023-04-22 21:29:12 +0200

    Added buttons to the form.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 function NewExpense(props) {
+    const [isEditing, setIsEditing] = useState(false);
+
+
     const saveExpenseDateHandler = (enteredExpenseDate) => {
         const expenseDate = {
             ...enteredExpenseDate,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseDate);
+        setIsEditing(false)
     }
+
+    const startEditingHandler = () => {
+        setIsEditing(true)
+    }
+
+    const stopEditingHandler = () => {
+        setIsEditing(false)
+    }
+
     return <div className="new-expense">
-        <ExpenseForm onSaveExpenseDate={saveExpenseDateHandler} ></ExpenseForm>
+        {!isEditing && <button onClick={startEditingHandler}>Add new expense</button>}
+        {isEditing && <ExpenseForm onSaveExpenseDate={saveExpenseDateHandler} onCancel={stopEditingHandler}></ExpenseForm>}
     </div>
 }
 
